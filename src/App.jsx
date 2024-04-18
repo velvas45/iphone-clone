a7b4d429293d5a02dd7dec848309f6592bfc8ee5
helmi.agustiawan <helmi@bitcoin.co.id>
2024-04-18 18:08:06 +0700

    iphone website ready to go

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import Chips from "./components/Chips";
+import Features from "./components/Features";
+import Footer from "./components/Footer";
+import Hero from "./components/Hero";
+import Highlights from "./components/Highlights";
+import Model from "./components/Model";
+import Navbar from "./components/Navbar";
+import * as Sentry from "@sentry/react";
+
+function App() {
+  return (
+    <main className="bg-black">
+      <Navbar />
+      <Hero />
+      <Highlights />
+      <Model />
+      <Features />
+      <Chips />
+      <Footer />
+    </main>
+  );
+}
+
+export default Sentry.withProfiler(App);
